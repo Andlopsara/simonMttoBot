b44d23ef3f892d9b9cea80898fdb5894495bbf67
Andrea López <guerrerosarahi64@gmail.com>
2025-07-09 13:24:59 -0600

    cree bot y nuevo archivo

---
+++ base-ts-baileys-mongo/src/simonBot/welcomeFlow.ts
@@ -0,0 +1,26 @@
+import { addKeyword, EVENTS } from "@builderbot/bot";
+
+export const welcomeFlow = addKeyword(EVENTS.WELCOME || EVENTS.VOICE_NOTE )
+    .addAnswer(`*¡Hola! 👋*, Gracias por contactarme. `)
+    .addAnswer("Estoy muy interesada en la oportunidad de formar parte de TREMEC 🤝, por lo que decidí realizar este MVP 🚀 para que puedan conocer mis habilidades 💻.")
+    /*.addAnswer(
+        [
+            'Por favor, elige una opción:',
+            '1️⃣ 📄 Currículum - Te comparto mi CV actualizado.',
+            '2️⃣ 🤖 Demo del Tremec Bot - Te muestro cómo funciona.',
+            '3️⃣ 🚪 Salir - Finalizar conversación.',
+            ' ',
+            'Espero tu respuesta. ¡Saludos! 😊'
+        ].join('\n'),
+        { capture: true },
+        async (ctx, { fallBack ,provider ,endFlow}) => {
+            const options = ["1", "2", "3"];
+            if(ctx.body === "3"){
+                return endFlow(commonMessag.endMessage);
+            }
+            if (!options.includes(ctx.body)){
+                return fallBack(commonMessag.selectOption(ctx.name))
+            }
+        },
+        [curriculumFlow, demoBotFlow]
+    )*/
